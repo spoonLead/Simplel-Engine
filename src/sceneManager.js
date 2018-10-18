64e280261c6f5a834595ab937dbb8998d0abc777
spoonLead <xuxlaevoleg@yandex.ru>
2018-10-18 21:01:53 +0400

    Add comments to addObject and rmObject methods of sceneManager

--- src/sceneManager.js
+++ src/sceneManager.js
@@ -3,7 +3,9 @@ class Scene{
     this.objects = [];
   }
 
+  //Add new object based on flatObject to the scene  
   addObject(objectName, object, x, y, width, height, physicality, layer){
+    //temp dictionary for push to buffer
     bufferObject = {
       objectName : objectName,
       object : object,
@@ -21,6 +23,7 @@ class Scene{
   }
 
   rmObject(objectName){
+    //find object with atribute objectName in scene buffer
     for(var i = 0; i < this.objects.lenght; i++){
       if(this.objects[i].objectName == objectName){
         this.objects.splice(i, 1);

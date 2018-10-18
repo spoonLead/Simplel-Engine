3101a68e6bab3843a5ec69013fecc45acee17610
spoonLead <xuxlaevoleg@yandex.ru>
2018-10-18 20:55:53 +0400

    Add method addObject to sceneManager

--- src/sceneManager.js
+++ src/sceneManager.js
@@ -1,14 +1,27 @@
 class Scene{
-  constructor{
-
+  constructor(){
+    this.objects = [];
   }
 
-  addObject(){
-
+  addObject(objectName, object, x, y, width, height, physicality, layer){
+    bufferObject = {
+      objectName : objectName,
+      object : object,
+      x : x,
+      y : y,
+      ex : x + width,
+      ey : y + height,
+      width : width,
+      height : height,
+      physicality : physicality,
+      layer : layer,
+      modelPath : object.modelPath,
+    }
+    objects.push(bufferObject);
   }
 
   rmObject(){
-
+    
   }
 
   start(){
@@ -16,7 +29,7 @@ class Scene{
   }
 
   stop(){
-    
+
   }
 
 }
